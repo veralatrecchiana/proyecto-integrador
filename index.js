3a9de4c4198723191983d624d9610331628198d4
veralatrecchiana <vlatrecchiana@udesa.edu.ar>
2024-11-08 11:00:37 -0300

    cree API

---
+++ index.js
@@ -0,0 +1,29 @@
+fetch('https://dummyjson.com/recipes')
+.then(res => res.json())
+.then(console.log);
+
+const recetasLista = document.querySelector(".recetasLista")
+let recetas = ""
+
+fetch('https://dummyjson.com/recipes')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data.recipes);
+        for(let i=0; i<data.recipes.length; i++){
+            const receta = data.recipes[i] //1 receta//
+            const markUp = `
+            <article class="recetas">
+                <img src="${receta.image}" alt="${receta.name}">
+                <p>Name: ${receta.name}</p>
+                <p>Status: ${receta.status}</p>
+            </article>`;
+            recetas += markUp
+        }
+        recetasLista.innerHTML = recetas;
+    })
+
+    .catch(function(error){
+        console.log("Mi error fue ", error)
+    });
